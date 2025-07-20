647131694a75af613073837ef0ed5d5d75c4b86f
ruhanirin999 <yumiella990@gmail.com>
2025-07-20 20:49:21 +0600

    ok

---
+++ watermelon.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main (){
+    
+    int w;
+    
+    printf("Enter the value:");
+    scanf("%d",&w);
+    
+    if(w>=4 && w<=100 && w%2==0)  
+      
+       {
+         printf("YES\n");
+       } 
+    else
+   
+       {
+         printf("NO\n");
+       }      
+    
+    return 0;
+   
+}
