72da0fa4bb77d3e3c35b8b299f6c8f0e25744e89
ruhanirin999 <yumiella990@gmail.com>
2025-07-27 17:25:14 +0600

    ok

--- watermelon.c
+++ watermelon.c
@@ -1,22 +1,21 @@
-#include<stdio.h>
-int main (){
-    
+#include <stdio.h>
+
+int main() {
     int w;
+
+    printf("Enter the value: ");
+    scanf("%d", &w);
+
+    if (w >= 4 && w <= 100 && w % 2 == 0)
+     {
+        printf("YES\n");
+     }
     
-    printf("Enter the value:");
-    scanf("%d",&w);
-    
-    if(w>=4 && w<=100 && w%2==0)  
-      
-       {
-         printf("YES\n");
-       } 
     else
-   
-       {
-         printf("NO\n");
-       }      
     
+    {
+        printf("NO\n");
+    }
+
     return 0;
-   
 }
